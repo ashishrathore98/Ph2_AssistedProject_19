d083a5d5860960fb56a7aaa6614e7a0c84136925
Ashish Rathore <ashrathore98@gmail.com>
2023-09-05 02:12:48 +0530

    first commit

---
+++ AssistedProject_19/Models/StudentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssistedProject_19.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string WhichClass { get; set; }
+        public string Address { get; set; }
+        public int TotalMarks { get; set; }
+
+
+    }
+
+}
